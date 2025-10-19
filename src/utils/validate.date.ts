a9031d351cd5a91e0e2960e535ec3b1b02f36f7d
Natalia Henao <nataliahenaor24@gmail.com>
2025-10-19 02:19:59 -0500

    feat: add business logic validation for endDate field

---
+++ src/utils/validate.date.ts
@@ -0,0 +1,27 @@
+import type { ProjectStatus } from "../interfaces/project.interface.js";
+import { AppError } from "./app.error.js";
+
+export const DateIsRequired = (
+  status: ProjectStatus,
+  date?: string
+): Date | null => {
+  const endDateValue = date ? new Date(date) : null;
+  const hasEndDate = endDateValue !== null && endDateValue !== undefined;
+
+  if (status === "completed" && !hasEndDate) {
+    throw new AppError(
+      "End date is required when status is Completed",
+      400,
+      "ValidationError"
+    );
+  }
+
+  if (status === "in progress" && hasEndDate) {
+    throw new AppError(
+      "End date is not allowed when status is In Progress",
+      400,
+      "ValidationError"
+    );
+  }
+  return endDateValue;
+};
